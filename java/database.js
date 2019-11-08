2a3cf4a93f6c040b04f255b928ab2261ddc8a7d7
Obregon1993 <cjangie13@gmail.com>
2019-11-08 14:20:33 -0500

    check

---
+++ java/database.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDQzYEcp_O-xp_FfLT842gcOy-BJTE1Qgk",
+    authDomain: "argon-3fe97.firebaseapp.com",
+    databaseURL: "https://argon-3fe97.firebaseio.com",
+    projectId: "argon-3fe97",
+    storageBucket: "argon-3fe97.appspot.com",
+    messagingSenderId: "621276750798",
+    appId: "1:621276750798:web:b9ed551917db5f283cc19f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  function increaseCounter(number) {
+      
+      console.log("works"+number)
+  }
